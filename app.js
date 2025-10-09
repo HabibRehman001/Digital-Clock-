80c461aa324da226bf123a311b8fa3c54ef6806b
HabibRehman001 <khuwajaabdulrehman046@gmail.com>
2025-10-10 01:09:15 +0500

    updated

--- app.js
+++ app.js
@@ -4,17 +4,23 @@ setInterval(() => {
     //getting date
     let getDate = new Date();
     //setting day
-spanDays.innerText = daysArray[getDate.getDay()]
+spanDays.innerText = daysArray[getDate.getDay()];
    //setting date
-let date = document.querySelector('.date')
-date.innerText = getDate.getDate()
+let date = document.querySelector('.date');
+date.innerText = getDate.getDate();
 //setting year
-let year = document.querySelector('.year')
-year.innerText = getDate.getFullYear()
+let year = document.querySelector('.year');
+year.innerText = getDate.getFullYear();
+//setting hours
 let hr = document.querySelector('.hr');
-hr.innerText = getDate.getHours()-12
+hr.innerText = getDate.getHours();
+if (hr.innerText >12) {
+    hr.innerText = hr.innerText -12
+};
+//setting minutes
 let min = document.querySelector('.min');
 min.innerText = getDate.getMinutes();
+//setting seconds
 let sec = document.querySelector('.sec');
 sec.innerText = getDate.getSeconds();
-},1000)
+},1000);
